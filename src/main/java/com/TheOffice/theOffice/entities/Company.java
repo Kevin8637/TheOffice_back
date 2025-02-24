d65ce9d98380ada39243423898048d4cb86bfd49
Kevin8637 <kevin.lar@live.fr>
2025-02-24 14:36:20 +0100

    Add Cycle entity + update company entity

--- src/main/java/com/TheOffice/theOffice/entities/Company.java
+++ src/main/java/com/TheOffice/theOffice/entities/Company.java
@@ -7,15 +7,17 @@ public class Company {
     String sector;
     String name;
     Date creation_date;
+    Long id_user;
 
     public Company (){
     }
 
-    public Company(Long id, String sector, String name, Date creation_date) {
+    public Company(Long id, String sector, String name, Date creation_date, Long id_user) {
         this.id = id;
         this.sector = sector;
         this.name = name;
         this.creation_date = creation_date;
+        this.id_user = id_user;
     }
 
     public Long getId() {
@@ -48,5 +50,13 @@ public class Company {
 
     public void setCreation_date(Date creation_date) {
         this.creation_date = creation_date;
+    }
+
+    public Long getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(Long id_user) {
+        this.id_user = id_user;
     }
 }
