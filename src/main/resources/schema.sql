a146472876419612df055ee69cb6b342dd3887f7
Kevin8637 <kevin.lar@live.fr>
2025-02-25 14:57:59 +0100

    New Update of schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS USER
+CREATE TABLE IF NOT EXISTS User
 (
     id       INT AUTO_INCREMENT     NOT NULL PRIMARY KEY,
     email    VARCHAR(255) UNIQUE    NOT NULL,
@@ -8,17 +8,17 @@ CREATE TABLE IF NOT EXISTS USER
     wallet    DECIMAL(10, 2)     NOT NULL DEFAULT 0.00
 );
 
-CREATE TABLE IF NOT EXISTS COMPANY
+CREATE TABLE IF NOT EXISTS Company
 (
     id            INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     sector       VARCHAR(255)       NOT NULL,
     name          VARCHAR(255)       NOT NULL,
     creation_date DATE               NOT NULL,
     id_user       INT,
-    FOREIGN KEY (id_user) REFERENCES USER (id)
+    FOREIGN KEY (id_user) REFERENCES User (id)
 );
 
-CREATE TABLE IF NOT EXISTS LOAN
+CREATE TABLE IF NOT EXISTS Loan
 (
     id            INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     loan_amount   DECIMAL(10, 2)     NOT NULL,
@@ -26,20 +26,20 @@ CREATE TABLE IF NOT EXISTS LOAN
     duration      INT                NOT NULL,
     rest DECIMAL(10, 2)     NOT NULL,
     id_user       INT,
-    FOREIGN KEY (id_user) REFERENCES USER (id)
+    FOREIGN KEY (id_user) REFERENCES User (id)
 );
 
-CREATE TABLE IF NOT EXISTS SUPPLIER
+CREATE TABLE IF NOT EXISTS Supplier
 (
     id         INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name       VARCHAR(255)       NOT NULL,
     price      DECIMAL(10, 2)     NOT NULL,
     quality    VARCHAR(100)       NOT NULL,
     id_company INT,
-    FOrEIGN KEY (id_company) REFERENCES COMPANY (id)
+    FOrEIGN KEY (id_company) REFERENCES Company (id)
 );
 
-CREATE TABLE IF NOT EXISTS CYCLE
+CREATE TABLE IF NOT EXISTS Cycle
 (
     id           INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     cost         DECIMAL(10, 2)     NOT NULL,
@@ -48,10 +48,10 @@ CREATE TABLE IF NOT EXISTS CYCLE
     popularity   INT                NOT NULL,
     step         INT                NOT NULL,
     id_company   INT,
-    FOREIGN KEY (id_company) REFERENCES COMPANY (id)
+    FOREIGN KEY (id_company) REFERENCES Company (id)
 );
 
-CREATE TABLE IF NOT EXISTS MACHINE
+CREATE TABLE IF NOT EXISTS Machine
 (
     id               INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name             VARCHAR(255)       NOT NULL,
@@ -60,16 +60,16 @@ CREATE TABLE IF NOT EXISTS MACHINE
     maintenance_cost DECIMAL(10, 2)     NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS MACHINE_IN_COMPANY
+CREATE TABLE IF NOT EXISTS MachineInCompany
 (
     id_machine INT,
     id_company INT,
     PRIMARY KEY (id_machine, id_company),
-    FOREIGN KEY (id_machine) REFERENCES MACHINE (id),
-    FOREIGN KEY (id_company) REFERENCES COMPANY (id)
+    FOREIGN KEY (id_machine) REFERENCES Machine (id),
+    FOREIGN KEY (id_company) REFERENCES Company (id)
 );
 
-CREATE TABLE IF NOT EXISTS EMPLOYEE
+CREATE TABLE IF NOT EXISTS Employee
 (
     id     INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name   VARCHAR(255)       NOT NULL,
@@ -83,16 +83,16 @@ CREATE TABLE IF NOT EXISTS EMPLOYEE
     health INT
 );
 
-CREATE TABLE IF NOT EXISTS EMPLOYEE_IN_COMPANY
+CREATE TABLE IF NOT EXISTS EmployeeInCompany
 (
     id_employee INT,
     id_company  INT,
     PRIMARY KEY (id_employee, id_company),
-    FOREIGN KEY (id_employee) REFERENCES EMPLOYEE (id),
-    FOREIGN KEY (id_company) REFERENCES COMPANY (id)
+    FOREIGN KEY (id_employee) REFERENCES Employee (id),
+    FOREIGN KEY (id_company) REFERENCES Company (id)
 );
 
-CREATE TABLE IF NOT EXISTS LOCAL
+CREATE TABLE IF NOT EXISTS Local
 (
     id            INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     level         ENUM ('petit local', 'moyen local', 'grand local') NOT NULL DEFAULT'petit local',
@@ -101,39 +101,39 @@ CREATE TABLE IF NOT EXISTS LOCAL
     max_employees INT                NOT NULL,
     max_machines  INT                NOT NULL,
     id_company    INT,
-    FOREIGN KEY (id_company) REFERENCES COMPANY (id)
+    FOREIGN KEY (id_company) REFERENCES Company (id)
 );
 
-CREATE TABLE IF NOT EXISTS EVENT
+CREATE TABLE IF NOT EXISTS Event
 (
     id     INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     result VARCHAR(255)       NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS COMPANY_EVENT
+CREATE TABLE IF NOT EXISTS CompanyEvent
 (
     id_company INT,
     id_event   INT,
     PRIMARY KEY (id_company, id_event),
-    FOREIGN KEY (id_company) REFERENCES COMPANY (id),
-    FOREIGN KEY (id_event) REFERENCES EVENT (id)
+    FOREIGN KEY (id_company) REFERENCES Company (id),
+    FOREIGN KEY (id_event) REFERENCES Event (id)
 );
 
-CREATE TABLE IF NOT EXISTS STOCK_MATERIAL
+CREATE TABLE IF NOT EXISTS StockMaterial
 (
     id            INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name          VARCHAR(255)       NOT NULL,
     quantity      INT                NOT NULL,
     id_company    INT,
-    FOREIGN KEY (id_company) REFERENCES COMPANY (id)
+    FOREIGN KEY (id_company) REFERENCES Company (id)
 );
 
-CREATE TABLE IF NOT EXISTS STOCK_FINAL_MATERIAL
+CREATE TABLE IF NOT EXISTS StockFinalMaterial
 (
     id            INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name          VARCHAR(255)       NOT NULL,
     quality      INT                NOT NULL,
     quantity      INT                NOT NULL,
     id_company    INT,
-    FOREIGN KEY (id_company) REFERENCES COMPANY (id)
+    FOREIGN KEY (id_company) REFERENCES Company (id)
 );
